b2e6ba1443ee561a0077e9883b336a462efd759d
Sunetb <sunetb@gmail.com>
2020-02-19 18:56:07 +0100

    Er den klar til at modtage??

--- app/src/main/java/dk/stbn/eksperifcm/MainActivity.java
+++ app/src/main/java/dk/stbn/eksperifcm/MainActivity.java
@@ -1,8 +1,19 @@
 package dk.stbn.eksperifcm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +21,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Lav notifikationskanal
+        //fra androids eksemepel-app: https://github.com/firebase/quickstart-android/blob/master/messaging/app/src/main/java/com/google/firebase/quickstart/fcm/java/MainActivity.java
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
+
+        //Tjek acces-token
+        //fra android guide: https://firebase.google.com/docs/cloud-messaging/android/client
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("ExperiFCM", "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        System.out.println("Token: "+token);
+
+                        // Log and toast
+                        /*String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d("ExperiFCM", msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();*/
+                    }
+                });
+
+
+
 
     }
 }
